fb751b52f03a303a5b56405a967cf3cd15a6c585
islombek22 <zefscraft@gmail.com>
2023-05-30 12:21:25 +0500

    targetsdss

---
+++ app.js
@@ -0,0 +1,116 @@
+const herry=document.querySelector("#herry")
+const boxx1=document.querySelector(".boxx1")
+const bookshelf=document.querySelector(".bookshelf")
+const boxx10=document.querySelector(".boxx10")
+const boxx2=document.querySelector(".boxx2")
+const boxx3=document.querySelector(".boxx3")
+const boxx4=document.querySelector(".boxx4")
+const boxx5=document.querySelector(".boxx5")
+
+const boxx6=document.querySelector(".boxx6")
+const boxx7=document.querySelector(".boxx7")
+const boxx8=document.querySelector(".boxx8")
+const boxx9=document.querySelector(".boxx9")
+const k1=document.querySelector("#k1")
+const k2=document.querySelector("#k2")
+const k3=document.querySelector("#k3")
+const k4=document.querySelector("#k4")
+const k5=document.querySelector("#k5")
+const k6=document.querySelector("#k6")
+const k7=document.querySelector("#k7")
+const k8=document.querySelector("#k8")
+const k9=document.querySelector("#k9")
+
+
+const boxitem1=document.querySelector(".boxitem1")
+const boxitem2=document.querySelector(".boxitem2")
+
+
+
+herry.addEventListener("click",()=>{
+    boxx1.style = "display:block; ";
+   
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  k1.addEventListener("click",()=>{
+    boxx2.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  k2.addEventListener("click",()=>{
+    boxx3.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  k3.addEventListener("click",()=>{
+    boxx4.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  k4.addEventListener("click",()=>{
+    boxx5.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  k5.addEventListener("click",()=>{
+    boxx6.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  
+  k6.addEventListener("click",()=>{
+    boxx7.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  
+  
+  k7.addEventListener("click",()=>{
+    boxx8.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  
+  
+  k8.addEventListener("click",()=>{
+    boxx9.style = "display:block; ";
+    bookshelf.style = "display:none;";
+   
+   
+    
+  })
+
+  
+  
+  
+
+  
+  
+
+
+  
